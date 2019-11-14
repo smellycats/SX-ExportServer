44d8c1692cd75a010234df9d70271559c20c64dd
SmellyCat <1282399562@qq.com>
2019-11-14 10:08:39 +0800

    添加 基础文件

---
+++ app/config.py
@@ -0,0 +1,26 @@
+﻿# -*- coding: utf-8 -*-
+
+class Config(object):
+    # 密码 string
+    SECRET_KEY = 'thefatboy'
+    # 服务器名称 string
+    HEADER_SERVER = 'SX-ExportServer'
+    # 数据库连接 string
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123123123@127.0.0.1:3307/environ'
+    # 数据库连接 dict
+    SQLALCHEMY_BINDS = {}
+    # 连接池大小 int
+    #SQLALCHEMY_POOL_SIZE = 20
+    # 基础路径
+    BASE_PATH = '/home/export'
+    # 基础URL路径
+    BASE_URL_PATH = 'http://112.91.72.23:8099/files'
+
+
+class Develop(Config):
+    DEBUG = True
+
+
+class Production(Config):
+    DEBUG = False
+
---
+++ app/views.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+import time
+
+import arrow
+import tablib
+from flask import g, request, make_response, jsonify, abort
+
+from . import db, app, cache, logger, access_logger
+#from .models import *
+from . import helper
+
+
+@app.route('/')
+def index_get():
+    result = {
+        'exp': '%sexp' % (request.url_root)
+    }
+    header = {'Cache-Control': 'public, max-age=600'}
+    return jsonify(result), 200, header
+
+@app.route('/exp', methods=['POST'])
+def export_post():
+    if not request.json:
+        return jsonify({'message': 'Problems parsing JSON'}), 415
+    try:
+        type = request.json.get('type', 'xlsx')
+        data = tablib.Dataset(headers=request.json['headers'])
+        for i in request.json['datas']:
+            data.append(i)
+        now = arrow.now('PRC').format('YYYYMMDD')
+        name = '{0}/{1}/{2}.{3}'.format(app.config['BASE_PATH'], now, request.json['title'], type)
+        path = '{0}/{1}'.format(app.config['BASE_PATH'], now)
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        with open(name, 'wb') as f:
+            f.write(data.export(type))
+    except Exception as e:
+        logger.exception(e)
+        raise
+    return jsonify({'path': '{0}/{1}/{2}.{3}'.format(app.config['BASE_URL_PATH'], now, request.json['title'], type)}), 201
